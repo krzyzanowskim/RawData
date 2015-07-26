5cdda4f487005899b4b57ce0ddb2123ecc3b91f4
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2015-07-26 16:32:34 +0200

    Initial Commit

---
+++ RawData/RawData.h
@@ -0,0 +1,19 @@
+//
+//  RawData.h
+//  RawData
+//
+//  Created by Marcin Krzyzanowski on 26/07/15.
+//  Copyright © 2015 Marcin Krzyżanowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RawData.
+FOUNDATION_EXPORT double RawDataVersionNumber;
+
+//! Project version string for RawData.
+FOUNDATION_EXPORT const unsigned char RawDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RawData/PublicHeader.h>
+
+
